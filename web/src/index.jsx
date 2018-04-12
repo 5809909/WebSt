1a4dea91554c5bd6b03804180f593e07d3bcd06b
Viktar Krauchyk <5809909@tut.by>
2018-04-12 16:56:20 +0300

    react works stage-0

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,22 +1,6 @@
-// import React from "react";
-// import { render } from "react-dom";
-// import { TodoApp } from "./todo-app";
-//
-// const App = () => (
-//     <div>
-//         <TodoApp/>
-//     </div>
-// );
-//
-// render(<App />, document.getElementById("root"));
-import React from "react";
-import { render } from "react-dom";
-import Hello from './hello';
+import React from 'react';
+import {render}  from 'react-dom';
+import { TodoApp } from'./todo-app.jsx';
 
-const App = () => (
-    <div>
-        <Hello />
-    </div>
-);
+render(<TodoApp/>, document.getElementById("app"));
 
-render(<App />, document.getElementById("root"));
