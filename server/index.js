3f76bde18b4b3beb2213057a2a0877deb13fbec8
Viktar Krauchyk <5809909@tut.by>
2018-05-14 14:55:00 +0300

    server begin work

---
+++ server/index.js
@@ -0,0 +1,10 @@
+ express = require ('express');
+const app = express();
+
+app.get('/', (req, res) =>{
+	res.send('Hello world!')
+});
+
+const server = app.listen(8080,()=>{
+	console.log("Server is up and running on port 8080");
+});
